6c436d002f670e471c967b6722200203030a0789
Johannes Weiss <johannesweiss@apple.com>
2025-09-23 12:28:55 +0100

    fix smoke in docker

--- Scripts/smoke-in-docker.sh
+++ Scripts/smoke-in-docker.sh
@@ -3,12 +3,13 @@
 set -eu
 
 tmpdir=$(mktemp -d /tmp/swipr-smoke_XXXXXX)
+swift package resolve # Package.resolved is necessary for 'ro' mount to work
 docker run \
     -it --rm \
     -v "$PWD:$PWD:ro" \
     -v "$tmpdir":/output \
     -w "$PWD" \
     --memory $(( 1024 * 1024 * 1024 )) \
-    swift:6.0-noble Scripts/smoke-test.sh /output
+    swift:6.0-noble bash -c 'export DEBIAN_FRONTEND=noninteractive && apt-get update && apt-get install -y curl && Scripts/smoke-test.sh /output'
 echo "$tmpdir"
 ls "$tmpdir"
